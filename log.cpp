20f0de9c2251df4c236c5b5db9c22f253daa2026
rabbitL <494496873@qq.com>
2015-06-04 22:20:50 +0800

    write log.h log.cpp

--- log.cpp
+++ log.cpp
@@ -4,33 +4,124 @@
   > Mail: ma6174@163.com 
   > Created Time: Mon 01 Jun 2015 09:03:51 PM CST
  ************************************************************************/
+#include "log.h"
+
 #include <stdio.h>
 #include <time.h>
 #include <sys/time.h>
-int main()
+#include <pthread.h>
+#include <unistd.h>
+
+
+LOG::LOG()
 {
 
-	struct timeval start;
-	struct tm ptr = {0};
-	gettimeofday(&start, NULL);
-	localtime_r(&start.tv_sec, &ptr);
-	char s[50];
-	strftime(s, 50, "%F-%H-%M-%S", &ptr);
-	printf("%s\n", s);
-	FILE *fp = fopen("log.txt", "a+");
+	sem_init(&sem, 0, 0);
+	flag = true;
+	isWriting = false;
+	fp = fopen("log.txt", "a+");
 	if(fp == NULL)
 	{
 		printf("fopen failed\n");
-		return 0;
+	}
+}
+
+void LOG::append(string str)
+{
+	printf("LOG::append\n");
+	if(!flag)
+	{
+		printf("buf\n");
+		buf.push_back(str);
+		if(buf.size() > 5 && !isWriting)
+		{
+			flag = true;
+			isWriting = true;
+			sem_post(&sem);
+		}
+	}
+	else
+	{
+		printf("buf1\n");
+		buf1.push_back(str);
+		if(buf1.size() > 5 && !isWriting)
+		{
+			flag = false;
+			isWriting = true;
+			sem_post(&sem);
+		}
+	}
+}
+
+void LOG::writebuf(string str)
+{
+	printf("%s\n", str.c_str());
+	if(fputs(str.c_str(), fp) == EOF)
+	{
+		perror("error : ");
+		printf("fput failed\n");
+	}
+}
+
+void LOG::write()
+{
+	if(flag)
+	{
+		while(!buf.empty())
+		{
+			writebuf("buf\n");
+			buf.pop_front();
+		}
+	}
+	else
+	{
+		while(!buf1.empty())
+		{
+			writebuf("buf1\n");
+			buf1.pop_front();
+		}
+	}
+	sync();
+	isWriting = false;
+}
+
+void *thread(void *arg)
+{
+	while(1)
+	{
+		LOG *log = (LOG *)arg;
+		sem_wait(&log->sem);
+		printf("log->write()\n");
+		log->write();
+	}
+}
+
+int main()
+{
+	LOG log;
+	string str("hello world!\n");
+	int arg1 = 1;
+	pthread_t th;
+	int *thread_ret = NULL;
+	int ret = pthread_create( &th, NULL, thread, &log );  
+	if( ret != 0 ){  
+		printf( "Create thread error!\n");  
+		return -1; 
+	} 
+
+//for(int i = 0; i < 5; ++i)
+	while(1)
+	{
+		printf("append log\n");
+		for(int i = 0; i < 10; ++i)
+		{
+			log.append(str);
+		}
+	//	sleep(2);
 	}
 
-	char *buf = "hello world!\n";
-	for(int i = 0; i < 5; ++i)
-		if(fputs(buf, fp) == EOF)
-		{
-			perror("error : ");
-			printf("fput failed\n");
-		}
+//	pthread_join( th, (void**)&thread_ret ); 
 
 	return 0;
 }
+
---
+++ log.h
@@ -0,0 +1,34 @@
+/*************************************************************************
+	> File Name: log.h
+	> Author: ma6174
+	> Mail: ma6174@163.com 
+	> Created Time: Tue 02 Jun 2015 08:55:38 PM CST
+ ************************************************************************/
+#ifndef _LOG_H_
+#define	_LOG_H_
+
+#include <deque>
+#include <string>
+#include <semaphore.h>
+#include <stdio.h>
+#include <pthread.h>
+using namespace std;
+
+class LOG
+{
+	public:
+		LOG();
+		void append(string str);
+		void writebuf(string str);
+		void write();
+	private:
+		deque<string> buf, buf1;
+		bool flag;
+	public:
+		sem_t sem;
+	private:
+		bool isWriting;
+		FILE *fp;
+};
+
+#endif
